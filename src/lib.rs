2d4ba43071df1f42cc4ef9cc656c8b30dfd63ff7
khamutov <khamutov@ailabs.ru>
2023-12-03 19:02:37 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod sigmf {
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+#[cfg(test)]
+mod test;
---
+++ src/test.rs
@@ -0,0 +1,6 @@
+use crate::sigmf::*;
+
+#[test]
+fn test_add() {
+    assert_eq!(add(1, 2), 3);
+}
